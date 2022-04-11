4e6f22e5daab9fa45b03450180e891eb3d860fbd
Yizhou Wang <wyzh555@gmail.com>
2022-04-11 00:59:50 -0700

    add evaluation function for cruw2022

--- cruw/eval/__init__.py
+++ cruw/eval/__init__.py
@@ -1,2 +1,3 @@
 from .rod.eval_rod2021 import evaluate_rod2021
 from .rod.eval_rodnet import evaluate_rodnet_seq
+from .rod.eval_cruw2022 import evaluate_rodnet_cruw2022
---
+++ cruw/eval/rod/eval_cruw2022.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+
+from .load_txt import read_gt_txt, read_sub_txt, read_rodnet_res
+from .rod_eval_utils import compute_ols_dts_gts, evaluate_img, accumulate, summarize
+
+olsThrs = np.around(np.linspace(0.5, 0.9, int(np.round((0.9 - 0.5) / 0.05) + 1), endpoint=True), decimals=2)
+recThrs = np.around(np.linspace(0.0, 1.0, int(np.round((1.0 - 0.0) / 0.01) + 1), endpoint=True), decimals=2)
+
+
+def evaluate_rodnet_cruw2022(res_dets, gt_dets, n_frame, dataset):
+
+    olss_all = {(imgId, catId): compute_ols_dts_gts(gt_dets, res_dets, imgId, catId, dataset) \
+                for imgId in range(n_frame)
+                for catId in range(3)}
+
+    evalImgs = [evaluate_img(gt_dets, res_dets, imgId, catId, olss_all, olsThrs, recThrs, dataset)
+                for imgId in range(n_frame)
+                for catId in range(3)]
+
+    return evalImgs
+
